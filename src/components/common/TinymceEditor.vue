81332cea05e34c2764f4cfabe3adc92103bab8f7
WangDaqian <wang_daqian11@nhn-st.com>
2022-01-21 10:52:02 +0800

    add plugins test

--- src/components/common/TinymceEditor.vue
+++ src/components/common/TinymceEditor.vue
@@ -1,32 +1,36 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-// import tinymce from "tinymce/tinymce"
-// tinymce.init
 
-import Editor from "@tinymce/tinymce-vue"
+import Editor from '@tinymce/tinymce-vue'
 
 import '@/assets/tinymce/langs/zh_CN.js'
-import 'tinymce/themes/silver/theme'
 
-import 'tinymce/plugins/image'
+import '@/assets/tinymce/plugins/code/plugin.min.js'
+import '@/assets/tinymce/plugins/image/plugin.min.js'
+import '@/assets/tinymce/plugins/axupimgs/plugin.min.js'
+import '@/assets/tinymce/plugins/media/plugin.min.js'
+import '@/assets/tinymce/plugins/emoticons/plugin.min.js'
+import '@/assets/tinymce/plugins/hr/plugin.min.js'
+import '@/assets/tinymce/plugins/table/plugin.min.js'
 
-import 'tinymce/plugins/table'
-import 'tinymce/plugins/lists'
-import 'tinymce/plugins/wordcount'
-import 'tinymce/plugins/colorpicker'
-import 'tinymce/plugins/code'
-import 'tinymce/plugins/paste'
+import '@/assets/tinymce/plugins/charmap/plugin.min.js'
+import '@/assets/tinymce/plugins/link/plugin.min.js'
+
+import '@/assets/tinymce/plugins/tinymce-plugin/plugin.min.js'
 
 const content = ref('<p>default</p>')
 
 const init = {
-  language: "zh_CN",
+  language: 'zh_CN',
   // width: 600,
   height: 430,
   resize: false,
   menubar: false,
-  plugins: 'image media',
-  toolbar: ['image media', 'formatselect']
+  plugins: 'code image axupimgs media emoticons hr table charmap link tinymce-plugin',
+  toolbar: [
+    'code image axupimgs media emoticons hr table tinymce-plugin',
+    'formatselect fontsizeselect | bold italic underline strikethrough subscript | align lineheight | charmap link'
+  ]
 }
 
 </script>
